74c3c44ccc2c93192e245233359d24c42bd56df7
= <dsilvester00@gmail.com>
2018-03-10 11:17:46 +0000

    Gonna stick it on github now

---
+++ README.md
@@ -0,0 +1,5 @@
+### Pinkbike PoD Fetch
+
+Downloads the Pinkbike photo of the day in its highest resolution. Works for now, however PB change their URLs every now and then so may need to be updated!
+
+Just modify `POD_DIR` if you want the photos to be saved somewhere else.
--- pod_fetch.py
+++ pod_fetch.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 import datetime
 
 PB_HOMEPAGE = "https://www.pinkbike.com"
-PHOTO_PREFIX = "https://www.pinkbike.com/photo/"
+PHOTO_PREFIX = PB_HOMEPAGE + "/photo/"
 POD_DIR = "pods/"
+
 
 def find_pod_url(soup):
     for a in soup.find_all('a', href=True):
@@ -13,12 +14,13 @@ def find_pod_url(soup):
             if len(url) == len(PHOTO_PREFIX) + 9:
                 return url
 
+
 def get_pod_img(url):
     pod_num = url[len(PHOTO_PREFIX):-1]
-    soup = BeautifulSoup(urlopen(url), 'html.parser')
     img_src = "http://ep1.pinkbike.org/p6pb" + pod_num + "/p0pb" + pod_num + ".jpg"
     pic = urlopen(img_src).read()
     write_to_file(pic)
+
 
 def write_to_file(bin):
     fname = POD_DIR + "pb_pod_" + datetime.date.today().strftime("%Y%m%d") + ".jpg"
@@ -26,11 +28,12 @@ def write_to_file(bin):
         f.write(bin)
         print("PoD saved to " + fname)
 
+
 def main():
     print("Fetching today's PoD...")
     pod_url = find_pod_url(BeautifulSoup(urlopen(PB_HOMEPAGE), 'html.parser'))
     get_pod_img(pod_url)
 
+
 if __name__ == "__main__":
     main()
-    
